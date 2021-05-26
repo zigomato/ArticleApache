ca6dbe0c0a382e3f41b202e5d8a3fc05811b4235
zigomato <zigomato.webmaster@gmail.com>
2021-05-26 10:43:58 +0200

    Create index.php

---
+++ SecuriserAvecApache/index.php
@@ -0,0 +1,11 @@
+<html lang='fr'>
+<header>
+	<meta charset="utf-8" />
+	<title>Sécuriser ses dossiers avec htaccess</title>
+</header>
+<body>
+	<h1>Accueil</h1>	
+	<p>Ceci est la page d'accueil</p>
+	<p>Lien vers la page administration <a href="/admin/">Administration</a>.</p>
+</body>
+</html>
